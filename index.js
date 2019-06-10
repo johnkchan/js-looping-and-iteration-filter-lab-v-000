dbca6d84f74e2670726b001d8dd4bcf98a216e0d
Jonathan Chan <johnkchan252@gmail.com>
2019-06-10 00:37:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 // Code your solution in this file
 function findMatching(drivers, str) {
-  return drivers.filter(function (driver) { return driver.toLowerCase() == str.toLowerCase() })
+  return drivers.filter(function(driver) { return driver.toLowerCase() == str.toLowerCase() })
 }
 
 function fuzzyMatch(drivers, str) {
-  let matchingDrivers = [];
-  
-  for (const driver of drivers) {
-    if (driver[0].toLowerCase() == str.toLowerCase()) {
-      matchingDrivers.push(driver);
-    }
-  }
-  
-  return matchingDrivers;
-}
+  return drivers.filter(function(driver) {return driver[0] == str })
 
 function matchName(drivers, str) {
   return drivers.filter(function(driver) { return driver.name == str });
