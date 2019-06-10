306b50879e8655084ee1b0f077263fed984b799a
Jonathan Chan <johnkchan252@gmail.com>
2019-06-10 00:38:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solution in this file
 function findMatching(drivers, str) {
-  return drivers.filter(function(driver) { return driver.toLowerCase() == str.toLowerCase() })
+  return drivers.filter(function(driver) { return driver.toLowerCase() == str.toLowerCase() });
 }
 
 function fuzzyMatch(drivers, str) {
-  return drivers.filter(function(driver) {return driver[0] == str })
+  return drivers.filter(function(driver) {return driver[0] == str });
 
 function matchName(drivers, str) {
   return drivers.filter(function(driver) { return driver.name == str });
