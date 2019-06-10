3f2362f1e1df92eb36d5498d3223b861811bc39c
Jonathan Chan <johnkchan252@gmail.com>
2019-06-10 00:22:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // Code your solution in this file
 function findMatching(drivers, str) {
-  let array
+  let matchingDrivers = [];
+  
   for (const driver of drivers) {
     if (driver.toLowerCase() == str.toLowerCase()){
-      
+      matchingDrivers.push(driver);
     }
   }
+  
+  return matchingDrivers;
 }
